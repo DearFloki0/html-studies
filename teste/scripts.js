45175020bec6d674ccc67cb00f8851864a21781f
Eduardo <eduardojlsj@gmail.com>
2025-03-11 19:40:54 -0300

    few more studying scripts

--- teste/scripts.js
+++
@@ -1,8 +0,0 @@
-function somar() {
-    let num1 = parseFloat(document.getElementById("num1").value)
-    let num2 = parseFloat(document.getElementById("num2").value)
-
-    let soma = num1 + num2;
-
-    document.getElementById("result").innerText = soma;
-}
